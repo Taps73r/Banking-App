e11756bd507d08df77684205984e8b83bbc2b6e8
Taps73r <povroznykmisha@gmail.com>
2024-08-20 12:53:38 +0300

    Project setup

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-ibm-plex-serif",
+});
+
+export const metadata: Metadata = {
+    title: "Bankaps",
+    description: "Bankaps is the newest banking platform.",
+    icons: {
+        icon: "/icons/logo.svg",
+    },
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} ${ibmPlexSerif.variable}`}>
+                {children}
+            </body>
+        </html>
+    );
+}
